2134deaf6b78870edcea8f70d700d8d393044540
KSHevelev <shevelev.kirill@gmail.com>
2020-09-12 12:43:41 +0300

    Изменил начальные скрипты таблиц

--- src/main/resources/db/14_users.sql
+++ src/main/resources/db/14_users.sql
@@ -1,18 +1,18 @@
 create table h2020.users
 (
   id_users          bigserial primary key,
-  dt_update         timestamp(6) default now() not null,
-  s_user_modify     varchar(100) not null,
-  id_org            bigint references h2020.organizations (id_org) not null,
-  s_firstname       varchar(255) not null,
-  s_secondname      varchar(255) not null,
+  dt_update         timestamp(6) default now(),
+  s_user_modify     varchar(100),
+  id_org            bigint references h2020.organizations (id_org),
+  s_firstname       varchar(255),
+  s_secondname      varchar(255),
   s_middlename      varchar(255),
   s_position        varchar(1000)
 );
 
 -- add comments to the table
 comment on table h2020.users
-  is 'курс валют ЦБ';
+  is 'пользователи';
 -- add comments to the columns
 comment on column h2020.users.id_users
   is 'id'; --автоинкрементное поле
