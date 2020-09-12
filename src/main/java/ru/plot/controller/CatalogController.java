4868b161d520405b75ccb724476054206f1836c2
ILazarev <ilazarev@ibs.ru>
2020-09-12 15:07:02 +0500

    Ilazarev - контроллер справочников

--- src/main/java/ru/plot/controller/CatalogController.java
+++ src/main/java/ru/plot/controller/CatalogController.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.plot.entity.CurrencyRate;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user/catalog")
@@ -12,6 +17,12 @@ public class CatalogController {
 
     @GetMapping("/CurrencyRates")
     public String table1Page(Model model) {
+
+        List<CurrencyRate> currencyRates = new ArrayList<>();
+        CurrencyRate rate = new CurrencyRate("R01010", "036", "AUD", "1", "Австралийский доллар", "54,5870");
+        currencyRates.add(rate);
+
+        model.addAttribute("currencyRates", currencyRates);
 
         return "/catalog/CurrencyRates";
     }
