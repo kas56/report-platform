c0bbed38a34eb8ac419b7e6e3dcc047f4af9dc37
SKonkov <skonkov@ibs.ru>
2020-09-12 20:26:19 +0300

    jaxb отчета

--- src/main/java/ru/plot/controller/CatalogController.java
+++ src/main/java/ru/plot/controller/CatalogController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import ru.plot.entity.CurrencyRate;
 import ru.plot.repo.CurrencyRateRepository;
 
@@ -19,6 +20,13 @@ public class CatalogController {
     @Autowired
     CurrencyRateRepository currencyRateRepository;
 
+    @RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
+    public String getIndexPage(Model model) {
+        model.addAttribute("message", "Noname foundation");
+
+        return "/catalog";
+    }
+
     @GetMapping("/CurrencyRates")
     public String table1Page(Model model) {
 
@@ -27,5 +35,10 @@ public class CatalogController {
         model.addAttribute("currencyRates", currencyRates);
 
         return "/catalog/CurrencyRates";
+    }
+
+    @GetMapping("/Bics")
+    public String bics(Model model) {
+        return "/catalog/Bics";
     }
 }
