a4999c93fd5e86cea280c6cf90ef1319aaedab1f
istvolov <IStvolov@ibs.ru>
2020-09-13 11:12:56 +0500

    Фронт. Вёрстка select'ора дял ЭЦП

--- src/main/resources/static/js/pages/login.js
+++ src/main/resources/static/js/pages/login.js
@@ -25,36 +25,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    enterBtn.addEventListener("click", function () {
-        // Кнопка войти
-        let emailField = authForm.querySelector(".user_field");
-        let passwordField = authForm.querySelector(".password_field");
-
-        helper.getHttpPromise({
-            method: "POST",
-            url: "/api/public/login/login",
-            contentType: "application/json",
-            jsonData: {
-                email: emailField.value,
-                password: passwordField.value
-            }
-        }).then(function (response) {
-            var data = JSON.parse(response);
-
-            if (data.success) {
-                window.location = "/";
-            } else {
-                let errorMessage = authForm.querySelector(".error_message");
-                errorMessage.innerHTML = "Не правильный логин или пароль";
-            }
-        }, function (error) {
-            console.log(error);
-            let errorMessage = authForm.querySelector(".error_message");
-            errorMessage.innerHTML = "Не правильный логин или пароль";
-        });
-
-    });
-
     authTypePanel.addEventListener("click", function (evt) {
 
         let target = evt.target;
